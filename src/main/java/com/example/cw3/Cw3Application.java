3dd959e3984bae0b1e63b6c73df3a660854fb4f8
Quincy Mafake <quincy.mafake@flash.co.za>
2023-01-25 23:30:03 +0200

    Data repositories working on first author and/or book respositories save() method call. ID not incrementing on secondary save() call

--- src/main/java/com/example/cw3/Cw3Application.java
+++ src/main/java/com/example/cw3/Cw3Application.java
@@ -1,38 +1,96 @@
 package com.example.cw3;
 
-
-
-import com.example.storage.services.FileSystemStorageService;
+import com.example.data.models.Author;
+import com.example.data.models.Book;
+import com.example.data.models.Publisher;
+import com.example.data.repositories.AuthorRepository;
+import com.example.data.repositories.BookRepository;
+import com.example.data.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import com.example.storage.StorageProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages="com.example")
-@EnableConfigurationProperties(StorageProperties.class)
 @EntityScan("com.example.data.models")
 @EnableJpaRepositories("com.example.data.repositories")
 
-
 public class Cw3Application implements CommandLineRunner{
-    @Resource
-    FileSystemStorageService storageService;
 
     public static void main(String[] args) {
         SpringApplication.run(Cw3Application.class, args);
 
     }
 
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public Cw3Application(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        storageService.deleteAll();
-        storageService.init();
 
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+
+        System.out.println("Eric = " + eric);
+        System.out.println("ddd = " + ddd);
+
+        System.out.println("Save author Eric details");
+        authorRepository.save(eric);
+//        bookRepository.save(ddd); // Above line already invoke an INSERT INTO both author & book tables. So this
+                                    // line causes a PK violation. as it does the same
+        publisherRepository.save(publisher);
+
+        System.out.println("Author count: " + authorRepository.count() );        // findAll().get(0));
+        System.out.println("Books count: " + bookRepository.count() );
+
+
+        //        List author_select = authorRepository.findAllAuthoustordWithJpql();
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "393949459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        Thread.sleep(3000);
+
+        System.out.println("Save author Rod details - NOT WORKING PROPERLY");
+
+//        authorRepository.save(rod); //TODO: The PK is not incrementing causing a PK violation
+//        bookRepository.save(noEJB);
+
+        System.out.println("Author count: " + authorRepository.count() );        // findAll().get(0));
+        System.out.println("Books count: " + bookRepository.count() );
+
+        System.out.println("Total Number of Books = " + bookRepository.count() );
+
+        System.out.println("Test" + authorRepository.findAll());
+        System.out.println("Test" + bookRepository.findAll());
     }
 
 }
--- src/main/java/com/example/data/repositories/Tr_mcc_codesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.data.repositories;
-
-import com.example.data.models.Tr_mcc_codes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Tr_mcc_codesRepo extends JpaRepository<Tr_mcc_codes, Integer> {
-}
